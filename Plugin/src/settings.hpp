5bdb4f0cc34dde69ad05c6bb58bbcd9e2cd6377d
vcpkg-action <gottyduke@hotmail.com>
2023-09-22 03:32:57 +0800

    feat: separate `Settings`

---
+++ Plugin/src/settings.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "DKUtil/Hook.hpp"
+#include "DKUtil/Config.hpp"
+
+using namespace DKUtil::Alias;
+
+class Settings : public dku::model::Singleton<Settings>
+{
+private:
+    TomlConfig mainConfig = COMPILE_PROXY(Plugin::SETTINGS_NAME);
+
+    Double fractionOfWeight{"fractionOfWeight"};
+
+public:
+    float getFractionOfWeight()
+    {
+        return static_cast<float>(*fractionOfWeight);
+    }
+
+    void Load() noexcept
+    {
+        static std::once_flag bound;
+        std::call_once(bound, [&]() { mainConfig.Bind<0.0, 10.0>(fractionOfWeight, 0.0); });
+
+        mainConfig.Load();
+    }
+
+    void Save() noexcept
+    {
+        mainConfig.Generate();
+        mainConfig.Write();
+    }
+};
